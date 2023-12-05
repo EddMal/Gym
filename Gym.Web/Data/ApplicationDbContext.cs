1e32a9edb8183ba9bcacc913ebcb7460f3e7f5f1
EddMal <107246914+EddMal@users.noreply.github.com>
2023-12-05 16:29:19 +0100

    Added startup types for bdContext

--- Gym.Web/Data/ApplicationDbContext.cs
+++ Gym.Web/Data/ApplicationDbContext.cs
@@ -1,17 +1,18 @@
 ﻿using Gym.Web.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Gym.Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser,IdentityRole,string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
 
-        public DbSet<GymClass> GymClasse { get; set; } = default!;
+        public DbSet<GymClass> GymClass { get; set; } = default!;
         //public DbSet<ApplicationUser> ApplicationUser { get; set; } = default!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
